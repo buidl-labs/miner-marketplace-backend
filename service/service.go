e2288837dd39b61f719530d991926d97c0f33acd
Rajdeep Bharati <rajdeepbh@protonmail.com>
2021-07-14 14:52:38 +0530

    Index storage deals every 3 hours

--- service/service.go
+++ service/service.go
@@ -44,6 +44,7 @@ func Indexer(DB *pg.DB, node lens.API) {
 
 func hourlyTasks(DB *pg.DB, node lens.API) {
 	MinerPageMessages(DB, node)
+	StorageDeals(DB, node)
 
 	var FILREP_MINERS string = "https://api.filrep.io/api/v1/miners"
 
